601021b3872d6df447458d473e9882c60192894c
ledkaa <ledkaa@gmai.com>
2023-07-01 20:16:17 +0100

    comment

--- DomesticAppliencesForum/src/main/java/com/telerikacademy/domesticappliencesforum/mappers/CommentMapper.java
+++ DomesticAppliencesForum/src/main/java/com/telerikacademy/domesticappliencesforum/mappers/CommentMapper.java
@@ -1,4 +1,20 @@
 package com.telerikacademy.domesticappliencesforum.mappers;
 
+import com.telerikacademy.domesticappliencesforum.models.dtos.CommentDto;
+import com.telerikacademy.domesticappliencesforum.services.UserService;
+
+import javax.xml.stream.events.Comment;
+
 public class CommentMapper {
+
+    private UserService userService;
+
+    public CommentMapper(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Comment fromCommentDto(CommentDto commentDto){
+     //   Comment comment=new Comment();
+        return null;
+    }
 }
