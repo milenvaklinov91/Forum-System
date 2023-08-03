8c97a6648e62e679f2e0443396398eb2d80f2766
championsocks <kokostanev20@gmail.com>
2023-08-03 16:33:18 +0300

    commentmvc update

--- DomesticAppliencesForum/src/main/java/com/telerikacademy/domesticappliencesforum/controllers/mvc/CommentMvcController.java
+++ DomesticAppliencesForum/src/main/java/com/telerikacademy/domesticappliencesforum/controllers/mvc/CommentMvcController.java
@@ -1,23 +1,26 @@
 package com.telerikacademy.domesticappliencesforum.controllers.mvc;
 
 import com.telerikacademy.domesticappliencesforum.controllers.AuthenticationHelper;
+import com.telerikacademy.domesticappliencesforum.exceptions.AuthorizationException;
+import com.telerikacademy.domesticappliencesforum.exceptions.EntityDuplicateException;
 import com.telerikacademy.domesticappliencesforum.exceptions.EntityNotFoundException;
+import com.telerikacademy.domesticappliencesforum.exceptions.UnauthorizedOperationException;
 import com.telerikacademy.domesticappliencesforum.mappers.CommentMapper;
 import com.telerikacademy.domesticappliencesforum.models.Comment;
-import com.telerikacademy.domesticappliencesforum.models.Post;
+import com.telerikacademy.domesticappliencesforum.models.User;
+import com.telerikacademy.domesticappliencesforum.models.dtos.CommentDto;
+import com.telerikacademy.domesticappliencesforum.models.dtos.PostDto;
 import com.telerikacademy.domesticappliencesforum.models.filterOptions.FilterOptionsComment;
-import com.telerikacademy.domesticappliencesforum.models.filterOptions.PostFilterOptions;
 import com.telerikacademy.domesticappliencesforum.repositories.CommentRepositoryImpl;
-import com.telerikacademy.domesticappliencesforum.repositories.VoteCommentRepositoryImpl;
 import com.telerikacademy.domesticappliencesforum.services.interfaces.CommentService;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,29 +28,83 @@ import java.util.List;
 public class CommentMvcController {
     private final CommentService commentService;
     private final CommentRepositoryImpl commentRepository;
+    private final AuthenticationHelper authenticationHelper;
+    private final CommentMapper modelMapper;
+
     @Autowired
     public CommentMvcController(CommentService commentService
-            ,CommentRepositoryImpl commentRepository) {
+            , CommentRepositoryImpl commentRepository, AuthenticationHelper authenticationHelper, CommentMapper modelMapper) {
         this.commentService = commentService;
         this.commentRepository = commentRepository;
-
+        this.authenticationHelper = authenticationHelper;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/{id}")
     public String showSingleComment(@PathVariable int id, Model model) {
         try {
             Comment comment = commentService.getCommentById(id);
-             model.addAttribute("comment", comment);
+            model.addAttribute("comment", comment);
             return "comment";
         } catch (EntityNotFoundException e) {
             model.addAttribute("error", e.getMessage());
             return "not-found";
         }
     }
+
     @GetMapping
     public String showAllComments(Model model) {
         List<Comment> comments = commentService.getAllComments(new FilterOptionsComment());
         model.addAttribute("comments", comments);
         return "comments";
     }
+
+    @GetMapping("/new")
+    public String showNewCommentPage(Model model, HttpSession session) {
+        CommentDto comment = (CommentDto) session.getAttribute("currentComment");
+        try {
+            authenticationHelper.tryGetCurrentUser(session);
+            if (comment == null) {
+                comment = new CommentDto();
+            } else {
+                session.removeAttribute("currentComment");
+            }
+
+        } catch (AuthorizationException e) {
+            return "redirect:auth/login";
+        }
+        model.addAttribute("comment", comment);
+        return "comment-new";
+    }
+
+    @PostMapping("/new")
+    public String createNewComment(@Valid @ModelAttribute("comment") CommentDto commentDto, BindingResult errors,
+                                   Model model, HttpSession session) {
+        User user;
+        try {
+            user = authenticationHelper.tryGetCurrentUser(session);
+        } catch (AuthorizationException e) {
+            return "redirect:auth/login";
+        }
+        if (errors.hasErrors()) {
+            return "comment-new";
+        }
+
+        try {
+            Comment newComment = modelMapper.fromCommentDto(commentDto);
+            commentService.create(newComment, user);
+            return "redirect:/comments";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("error", e.getMessage());
+            return "not-found";
+        } catch (EntityDuplicateException e) {
+            errors.rejectValue("name", "duplicate_comment", e.getMessage());
+            return "comment-new";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("error", e.getMessage());
+            return "AccessDeniedView";
+        }
+    }
+
 }
+
